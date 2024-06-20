a881801642ed0a51c95af9d2d469993fbd14209e
gpaccip <gpaccip@unjbg.edu.pe>
2024-06-19 19:15:39 -0500

    actualizacion 3 del ejercicio 1

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -8,24 +8,23 @@ using namespace std;
 int main(){
 	
 	double a,b,c;
-	cout<<"\tENCONTRANDO EL NUMERO MAYOR"<<endl;
-	cout<<"Ingrese numeros diferentes: \n";
-	cout<<"Digite tres numeros: "<<endl;
-	cin>>a>>b>>c;
+	cout<<"\tENCONTRANDO EL MAYOR VALOR"<<endl;
+	cout<<"Ingrese valores diferentes \n";
+	cout<<"Digite tres numeros: "<<endl;cin>>a>>b>>c;
 	
-	if ((b!=c)&&(a!=b)&&(a!=c)){	
+	if ((b!=c)&&(a!=b)||(a!=c)){	
 		if((a>b)&&(a>c)){
-			cout<<a<<" es el numero mayor"<<endl;
+			cout<<a<<" es el mayor valor"<<endl;
 			}
 		else if((b>a)&&(b>c)){
-			cout<<b<<" es el numero mayor"<<endl;
+			cout<<b<<" es el mayor valor"<<endl;
 			}
 		else{
-		cout<<c<<" es el numero mayor"<<endl;
+		cout<<c<<" es el mayor valor"<<endl;
 		}
 	}
 	else {
-		cout<<"No pueden ser numeros iguales"<<endl;
+		cout<<"Todos los numeros no deben ser iguales"<<endl;
 	}
 	
 	system("pause");
